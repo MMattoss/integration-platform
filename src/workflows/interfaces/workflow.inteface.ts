86959b875208d8bf2915e6723eb2f3704a020d73
Mateus Mattos <mattoss.barreto@gmail.com>
2026-03-05 10:27:42 -0300

    improvment to triggerConfig type

--- src/workflows/interfaces/workflow.inteface.ts
+++ src/workflows/interfaces/workflow.inteface.ts
@@ -1,7 +1,21 @@
-export type TriggerConfig = 
-  | { webhookPath: string; secretToken?: string }
-  | { cronExpression: string; timezone: string } 
-  | { userId: string };                          
+import { TriggerType } from "../enums/workflow.enum";
+
+export type TriggerConfig =
+  | {
+      type: TriggerType.WEBHOOK;
+      webhookPath: string;
+      method?: 'GET' | 'POST';
+      secretToken?: string;
+    }
+  | {
+      type: TriggerType.SCHEDULE;
+      cronExpression: string;
+      timezone: string;
+    }
+  | {
+      type: TriggerType.MANUAL;
+      userId?: string;
+    };                         
 
 export interface RetryPolicy {
   maxRetries: number;
